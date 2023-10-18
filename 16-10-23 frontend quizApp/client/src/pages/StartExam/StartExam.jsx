d7bcf8f9da76b80cbbcb4688f252a4c6a3254466
sravan <sravan.k@egradtutor.in>
2023-10-18 15:52:27 +0530

    .

--- 16-10-23 frontend quizApp/client/src/pages/StartExam/StartExam.jsx
+++ 16-10-23 frontend quizApp/client/src/pages/StartExam/StartExam.jsx
@@ -1,113 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
-import Navbar from '../../components/Navbar/Navbar'
-import './StartExam.css'
-import { Link,NavLink } from 'react-router-dom'
-import PhoneInput from 'react-phone-number-input'
+import React, { useState, useEffect } from "react";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+
+import "./StartExam.css";
+import logo from '../../images/logo.jpg'
+
+import { Link, NavLink } from "react-router-dom";
+import PhoneInput from "react-phone-number-input";
 // import LoginSignUpBox from '../../components/Login&SignUp/Login&SignUpBox';
 
-
-
-
-
 const StartExam = () => {
-    const myComponentStyle1 = {
-        // color: 'white'
-    }
-    const myComponentStyle2 = {
-        // color: 'white'
-    }
+  const myComponentStyle1 = {
+    // color: 'white'
+  };
+  const myComponentStyle2 = {
+    // color: 'white'
+  };
 
-    const [showAlert, setShowAlert] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-    const openAlert = () => {
-        setShowAlert(true);
-    };
-    const closeAlert = () => {
-        setShowAlert(false);
-    };
+  const openAlert = () => {
+    setShowAlert(true);
+  };
+  const closeAlert = () => {
+    setShowAlert(false);
+  };
 
-    const countries = [
-        { id: "1", name: "INDIA" },
-        { id: "2", name: "USA" }
-    ];
+  const countries = [
+    { id: "1", name: "INDIA" },
+    { id: "2", name: "USA" },
+  ];
 
-    const states = [
-        { id: "1", countryId: "1", name: "Haryana" },
-        { id: "2", countryId: "1", name: "Delhi" },
-        { id: "3", countryId: "1", name: "Telangana" },
-        { id: "4", countryId: "1", name: "Andhra Pradesh" },
-        { id: "5", countryId: "2", name: "Texas" },
-        { id: "6", countryId: "2", name: "California" }
-    ]
+  const states = [
+    { id: "1", countryId: "1", name: "Haryana" },
+    { id: "2", countryId: "1", name: "Delhi" },
+    { id: "3", countryId: "1", name: "Telangana" },
+    { id: "4", countryId: "1", name: "Andhra Pradesh" },
+    { id: "5", countryId: "2", name: "Texas" },
+    { id: "6", countryId: "2", name: "California" },
+  ];
 
-    const cities = [
-        { id: "1", stateId: "1", name: "Faridabad" },
-        { id: "2", stateId: "1", name: "Palwal" },
-        { id: "3", stateId: "2", name: "Mandi House" },
-        { id: "4", stateId: "2", name: "kalka Ji" },
-        { id: "5", stateId: "3", name: "Hyderabad" },
-        { id: "6", stateId: "3", name: "Gachibowli" },
-        { id: "1", stateId: "5", name: "Houston" },
-        { id: "2", stateId: "5", name: "Austin" },
-        { id: "3", stateId: "6", name: "Los Angeles" },
-        { id: "4", stateId: "6", name: "Son Diego" },
-        { id: "5", stateId: "4", name: "Vijyawada" },
-        { id: "6", stateId: "4", name: "Thirupati" }
-    ]
+  const cities = [
+    { id: "1", stateId: "1", name: "Faridabad" },
+    { id: "2", stateId: "1", name: "Palwal" },
+    { id: "3", stateId: "2", name: "Mandi House" },
+    { id: "4", stateId: "2", name: "kalka Ji" },
+    { id: "5", stateId: "3", name: "Hyderabad" },
+    { id: "6", stateId: "3", name: "Gachibowli" },
+    { id: "1", stateId: "5", name: "Houston" },
+    { id: "2", stateId: "5", name: "Austin" },
+    { id: "3", stateId: "6", name: "Los Angeles" },
+    { id: "4", stateId: "6", name: "Son Diego" },
+    { id: "5", stateId: "4", name: "Vijyawada" },
+    { id: "6", stateId: "4", name: "Thirupati" },
+  ];
 
-    const [country, setCountry] = useState([]);
-    const [state, setState] = useState([]);
-    const [city, setCity] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [state, setState] = useState([]);
+  const [city, setCity] = useState([]);
 
-    useEffect(() => {
-        setCountry(countries);
-    }, [])
+  useEffect(() => {
+    setCountry(countries);
+  }, []);
 
-    const handleCountry = (id) => {
-        const dt = states.filter(x => x.countryId === id);
-        setState(dt);
-    }
+  const handleCountry = (id) => {
+    const dt = states.filter((x) => x.countryId === id);
+    setState(dt);
+  };
 
-    const handleState = (id) => {
-        const dt = cities.filter(x => x.stateId === id);
-        setCity(dt);
-    }
- 
-    const [value, setValue] = useState()
+  const handleState = (id) => {
+    const dt = cities.filter((x) => x.stateId === id);
+    setCity(dt);
+  };
 
+  const [value, setValue] = useState();
 
-    return (
-        <div className='main_conatiner'>
-            <div><Navbar /></div>
-            {showAlert && (
-                <div id='divContent1'>
-                    {/* <LoginSignUpBox/> */}
-                    <div>
-                        <div className='login_close_container'><h4>LOGIN</h4><p><span id='close-icon' class="material-symbols-outlined" onClick={closeAlert}>
-                            close
-                        </span></p></div>
-                        <form action="" id='form'>
-                            <p>
-                                <h5>Mobile Number or Email :</h5>
-                                <input type="text" placeholder="Enter your Mobile Number or Email" />
-                            </p>
-                            <p>
-                                <h5>Password :</h5>
-                                <input type="text" placeholder="Enter your Password" />
-                            </p>
-                            <p><Link>Forgot Password?</Link></p>
-                        </form>
-                        <div>
-                            <button>Submit</button>
-                            <button>Don't Have Account? SIGNUP</button>
-                        </div>
-                    </div>
-                  
-                </div>
-            )}
-            <div id='divContent2'>
-                {/* <LoginSignUpBox/> */}
+  return (
+    <div className="main_conatiner">
+      <div>
+        {/* <Navbar /> */}
+        <div className='navbar-div'>
+      <img src={logo} alt="logo" width='250px' />
+
+      <div className='auth-btn'>
+        <div><button onClick={openAlert}>LOGIN</button></div>
+        <div><button>SIGNUP</button></div>
+      </div>
+      {/* <div>
+        {showAlert && (
+          <div id='divContent'>
+            <LoginSignUpBox/>
+            <div>
+              <div className='login_close_container'><h4>LOGIN</h4><p><span id='close-icon' class="material-symbols-outlined" onClick={closeAlertForLogin1}>
+                close
+              </span></p></div>
+              <form action="" id='form'>
+                <p>
+                  <h5>Mobile Number or Email :</h5>
+                  <input type="text" placeholder="Enter your Mobile Number or Email" />
+                </p>
+                <p>
+                  <h5>Password :</h5>
+                  <input type="text" placeholder="Enter your Password" />
+                </p>
+                <p><Link>Forgot Password?</Link></p>
+              </form>
+              <div>
+                <button>Submit</button>
+                <button>Don't Have Account? SIGNUP</button>
+              </div>
+            </div>
+
+          </div>
+        )}
+      </div> */}
+    </div>
+
+      </div>
+      {showAlert && (
+        <div id="divContent1">
+          {/* <LoginSignUpBox/> */}
+          <div>
+            <div className="login_close_container">
+              <h4>LOGIN</h4>
+              <p>
+                <span
+                  id="close-icon"
+                  class="material-symbols-outlined"
+                  onClick={closeAlert}
+                >
+                  close
+                </span>
+              </p>
+            </div>
+            <form action="" id="form">
+              <p>
+                <h5>Mobile Number or Email :</h5>
+                <input
+                  type="text"
+                  placeholder="Enter your Mobile Number or Email"
+                />
+              </p>
+              <p>
+                <h5>Password :</h5>
+                <input type="text" placeholder="Enter your Password" />
+              </p>
+              <p>
+                <Link>Forgot Password?</Link>
+              </p>
+            </form>
+            <div>
+              <button>Submit</button>
+              <button>Don't Have Account? SIGNUP</button>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* <div id='divContent2'>
+                
                 <div>
                     <div className='login_close_container'><h4>SIGNUP</h4><p><span id='close-icon' class="material-symbols-outlined" onClick={closeAlert}>
                         close
@@ -183,73 +232,107 @@ const StartExam = () => {
                     </div>
                 </div>
 
-            </div>
+            </div> */}
 
-
-            <div className='ls-bar-exam-page'>
-                <div className='left-sidebar-div'><LeftSidebar />
+      <div className="ls-bar-exam-page">
+        <div className="left-sidebar-div">
+          <LeftSidebar />
+        </div>
+        <div className="exam-middle-div">
+          <div>
+            <h3 className="exam-heading">JEE MAIN Full Test</h3>
+          </div>
+          <div className="home-main-page">
+            {/* <h2>JEE MAIN Full Test</h2> */}
+            <div>
+              <div>
+                <div className="header-div1">
+                  <p className="div-heading">Online Test for JEE Main</p>
                 </div>
-                <div className='exam-middle-div'>
-                    <div><h3 className='exam-heading'>JEE MAIN Full Test</h3></div>
-                    <div className='home-main-page'>
-                        {/* <h2>JEE MAIN Full Test</h2> */}
-                        <div>
-                            <div>
-                                <div className='header-div1'>
-                                    <p className='div-heading'>Online Test for JEE Main</p>
-                                </div>
-                                <div className='header-div2'>
-                                    <div className='header-links'>
+                <div className="header-div2">
+                  <div className="header-links">
+                    <NavLink activeclassname="active" className="content-link">
+                      Full Test
+                    </NavLink>
+                    <NavLink activeclassname="active" className="content-link">
+                      Subject Test
+                    </NavLink>
+                    <NavLink activeclassname="active" className="content-link">
+                      Chapter Test
+                    </NavLink>
+                    <NavLink activeclassname="active" className="content-link">
+                      Previous Years{" "}
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+              {/* <HomeHeader /> */}
+            </div>
+            <div className="test-card">
+              <div className="test-card-header">
+                <div className="test-contents1">
+                  <h3 className="jee-test-1">JEE MAIN 2024 TEST-1</h3>
+                  <span
+                    style={myComponentStyle2}
+                    class="material-symbols-outlined"
+                  >
+                    lock_open
+                  </span>
+                </div>
+                <p className="testCard-second-header">
+                  Available Till: 31 May,2024
+                </p>
+                <p className="free-title">FREE</p>
+              </div>
+              <div>
+                <div className="test-contents2">
+                  <span
+                    style={myComponentStyle1}
+                    class="material-symbols-outlined"
+                  >
+                    help
+                  </span>
+                  <p>90 Questions</p>
+                </div>
 
-                                        <NavLink activeclassname='active' className='content-link' >Full Test</NavLink>
-                                        <NavLink activeclassname='active' className='content-link'>Subject Test</NavLink>
-                                        <NavLink activeclassname='active' className='content-link' >Chapter Test</NavLink>
-                                        <NavLink activeclassname='active' className='content-link'>Previous Years </NavLink>
+                <div className="test-contents2">
+                  <span
+                    style={myComponentStyle1}
+                    class="material-symbols-outlined"
+                  >
+                    schedule
+                  </span>
+                  <p>180 Minutes</p>
+                </div>
 
+                <div className="test-contents2">
+                  <span
+                    style={myComponentStyle1}
+                    class="material-symbols-outlined"
+                  >
+                    trending_up
+                  </span>
+                  <p>300 Marks</p>
+                </div>
 
-                                    </div>
-                                </div>
-                            </div>
-                            {/* <HomeHeader /> */}
-                        </div>
-                        <div className='test-card'>
-                            <div className='test-card-header'>
-                                <div className='test-contents1'><h3 className='jee-test-1'>JEE MAIN 2024 TEST-1</h3><span style={myComponentStyle2} class="material-symbols-outlined">
-                                    lock_open
-                                </span></div>
-                                <p className='testCard-second-header'>Available Till: 31 May,2024</p>
-                                <p className='free-title'>FREE</p>
-                            </div>
-                            <div>
-                                <div className='test-contents2'><span style={myComponentStyle1} class="material-symbols-outlined">help</span><p>90 Questions</p></div>
-
-                                <div className='test-contents2'><span style={myComponentStyle1} class="material-symbols-outlined">
-                                    schedule
-                                </span><p>180 Minutes</p></div>
-
-                                <div className='test-contents2'><span style={myComponentStyle1} class="material-symbols-outlined">
-                                    trending_up
-                                </span><p>300 Marks</p></div>
-
-                                {/* <div className='test-btn'><button onClick={startCountdown} className='play-btn'><span class="material-symbols-outlined">
+                {/* <div className='test-btn'><button onClick={startCountdown} className='play-btn'><span class="material-symbols-outlined">
                         chevron_right
                     </span>Start Test</button></div> */}
-                                <div className='test-btn'>
-                                    <button className='play-btn' onClick={openAlert}><span class="material-symbols-outlined">
-                                        chevron_right
-                                    </span>Start Test</button>
+                <div className="test-btn">
+                  <button className="play-btn" onClick={openAlert}>
+                    <span class="material-symbols-outlined">chevron_right</span>
+                    Start Test
+                  </button>
 
-                                    {/* element={<Instructions />} */}
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
+                  {/* element={<Instructions />} */}
                 </div>
+              </div>
             </div>
-
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default StartExam
+export default StartExam;
